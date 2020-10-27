9218efc18de513d312cc16ccb796a26818b7d269
angel <angel@LAPTOP-4Q6UOD55>
2020-10-27 13:58:21 -0500

    modifique la ventana de login

--- src/prestamoequipos/GUI/controllers/LoginController.java
+++ src/prestamoequipos/GUI/controllers/LoginController.java
@@ -3,6 +3,8 @@ package prestamoequipos.GUI.controllers;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,8 +13,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -22,32 +27,121 @@ import javafx.stage.Stage;
  * @author ion
  */
 public class LoginController implements Initializable {
+    @FXML
+    private Label escudolb;
 
-    
     @FXML
     private Text welcomeText;
-
-    @FXML
-    private TextField passwordLoginTF;
 
     @FXML
     private TextField userLoginTF;
 
     @FXML
-    private Button loginBtn;
+    private Label AdvertenciausLB;
 
     @FXML
+    private Label AdvertenciapLB;
+
+    @FXML
+    private Label userLogoLB;
+
+    @FXML
+    private Label lockLogoLB;
+
+    @FXML
+    private PasswordField passwordLoginTF;
+
+    @FXML
+    private Button loginBtn;
+    
+    @FXML
     void loginBtnAction(ActionEvent event) throws IOException {
-        Parent newParent = FXMLLoader.load(getClass().getResource("../views/studentHome.fxml"));
-        Scene newScene = new Scene(newParent);
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        window.setScene(newScene);
-        window.show();
+        boolean Validacion = false;
+        if(String.valueOf(passwordLoginTF.getText()).length() == 0){
+            System.out.println("no hay contraseña");
+            AdvertenciapLB.setText("No se ha ingresado una contraseña");
+            AdvertenciapLB.setVisible(true);
+        }
+        if(userLoginTF.getText().length() == 0){
+            System.out.println("no hay usuario");
+            AdvertenciausLB.setText("No se ha ingresado un usuario");
+            AdvertenciausLB.setVisible(true);
+        }
+        if (AdvertenciapLB.isVisible() == false && AdvertenciausLB.isVisible() == false) {
+            if (Validacion) {
+                Parent newParent = FXMLLoader.load(getClass().getResource("../views/studentHome.fxml"));
+                Scene newScene = new Scene(newParent);
+                Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                window.setScene(newScene);
+                window.show();
+            } else {
+                AdvertenciapLB.setText("El usuario y la contraseña no coinciden");
+                AdvertenciapLB.setVisible(true);
+            }
+        }else{
+            if(AdvertenciausLB.isVisible() == false && AdvertenciapLB.isVisible() == true){
+                if(!(AdvertenciausLB.getText().equals("No se ha ingresado un usuario"))){
+                    AdvertenciapLB.setText("El usuario y la contraseña no coinciden");
+                    AdvertenciapLB.setVisible(true);
+                }
+            }
+        }
     }
+    
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        AdvertenciapLB.setVisible(false);
+        AdvertenciausLB.setVisible(false);
+        
+        userLoginTF.requestFocus();
+
+        AdvertenciapLB.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/resources/alert.png"))));
+        AdvertenciausLB.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/resources/alert.png"))));
+        escudolb.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/resources/Escudo_color_200.png"))));
+        userLogoLB.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/resources/user.png"))));
+        lockLogoLB.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/resources/lock.png"))));
+        
+
+        userLoginTF.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue, Boolean newPropertyValue) {
+                if (newPropertyValue) {
+                    System.out.println("Textfield on focus");
+                    AdvertenciausLB.setVisible(false);
+                } else {
+                    System.out.println("Textfield out focus");
+                    AdvertenciapLB.setVisible(false);
+                    if (userLoginTF.getText().length() != 0) {
+                        AdvertenciausLB.setVisible(false);
+                        System.out.println(userLoginTF.getText());
+                    } else {
+                        AdvertenciausLB.setText("No se ha ingresado un usuario");
+                        AdvertenciausLB.setVisible(true);
+                    }
+                }
+            }
+        });
+
+        passwordLoginTF.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue, Boolean newPropertyValue) {
+                if (newPropertyValue) {
+                    System.out.println("Textfield on focus pa");
+                    AdvertenciapLB.setVisible(false);
+                } else {
+                    System.out.println("Textfield out focus");
+                    AdvertenciapLB.setVisible(false);
+                    if (String.valueOf(passwordLoginTF.getText()).length() == 0) {
+                        AdvertenciapLB.setText("No se ha ingresado una contraseña");
+                        AdvertenciapLB.setVisible(true);
+                    } else {
+                        AdvertenciapLB.setVisible(false);
+                    }
+                }
+            }
+        });
+    }
     
 }
--- src/prestamoequipos/Main.java
+++ src/prestamoequipos/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 /**
  *
@@ -21,6 +22,8 @@ public class Main extends Application  {
         Parent root = FXMLLoader.load(getClass().getResource("GUI/views/login.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Prestamo de equipos de computo");
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/resources/logotipo_UN_16.png")));
         primaryStage.show();
     }
     
