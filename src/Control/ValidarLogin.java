6810d1c6c8727e5f4c065684262ecf5858042473
Julian Bustos <dbustos@unal.edu.co>
2020-10-26 23:40:24 -0500

    Revert "Revert "Revert "Revert "Logica 0.2 """"
    This reverts commit 284ae368193901d2f02f371c0b2ab5af5b5a4acb.

---
+++ src/Control/ValidarLogin.java
@@ -0,0 +1,21 @@
+/*
+package Control;
+
+public class validarLogin {
+    
+    private UsuarioDAO usuarioDao = new UsuarioDAO();
+
+    public validarLogin() {
+    }
+    
+    public boolean verificarLogin(Usuario usuario){
+        if(usuarioDao.leer(usuario)){
+            System.out.println("Bienvenido");
+            return true;
+        }else{
+            System.out.println("Datos incorrectos");
+            return false;
+        }
+    }
+}
+*/
