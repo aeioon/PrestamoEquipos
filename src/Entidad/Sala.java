668a276e68b07408fdeaa21692d7216eff8e952a
Gabriela <Gabriela@DESKTOP-MFKP9P6>
2020-10-30 20:53:42 -0500

    DAO

--- src/Entidad/Sala.java
+++ src/Entidad/Sala.java
@@ -3,16 +3,20 @@ package Entidad;
 
 public class Sala {
     private int id;
+    private String codigo;
     private String nombre;
+    private String tipo;
     private Edificio edificio;
     private Encargado encargado;
 
     public Sala() {
     }
 
-    public Sala(int id, String nombre, Edificio edificio, Encargado encargado) {
+    public Sala(int id, String codigo, String nombre, String tipo, Edificio edificio, Encargado encargado) {
         this.id = id;
+        this.codigo = codigo;
         this.nombre = nombre;
+        this.tipo = tipo;
         this.edificio = edificio;
         this.encargado = encargado;
     }
@@ -25,12 +29,28 @@ public class Sala {
         this.id = id;
     }
 
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
     public String getNombre() {
         return nombre;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 
     public Edificio getEdificio() {
@@ -47,5 +67,7 @@ public class Sala {
 
     public void setEncargado(Encargado encargado) {
         this.encargado = encargado;
-    } 
+    }
+
+    
 }
