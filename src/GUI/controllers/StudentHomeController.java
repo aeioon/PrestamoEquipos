6deab3b1151f5f0ce1507609e74df87f78cab034
Julian Bustos <dbustos@unal.edu.co>
2020-11-01 17:38:34 -0500

    Prueba 10

--- src/GUI/controllers/StudentHomeController.java
+++ src/GUI/controllers/StudentHomeController.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -63,46 +64,70 @@ public class StudentHomeController implements Initializable {
 
     @FXML
     void requestLoanBtnAction(ActionEvent event) throws IOException {
-        if (!cargarDatos.isActivo()) {
-            changeScene(event, "/GUI/views/loanRequest.fxml");
-        } else {
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            fxmlLoader.setLocation(getClass().getResource("/GUI/views/noBorrow.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 390, 210);
-            Stage stagePop = new Stage();
-            stagePop.setTitle("ERROR");
-            stagePop.getIcons().add(new Image(getClass().getResourceAsStream("/GUI/static/icons/error.png")));
-            stagePop.setScene(scene);
-            stagePop.showAndWait();
-        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            if (!cargarDatos.isActivo()) {
+                                changeScene(event, "/GUI/views/loanRequest.fxml");
+                            } else {
+                                FXMLLoader fxmlLoader = new FXMLLoader();
+                                fxmlLoader.setLocation(getClass().getResource("/GUI/views/noBorrow.fxml"));
+                                Scene scene = new Scene(fxmlLoader.load(), 390, 210);
+                                Stage stagePop = new Stage();
+                                stagePop.setTitle("ERROR");
+                                stagePop.getIcons().add(new Image(getClass().getResourceAsStream("/GUI/static/icons/error.png")));
+                                stagePop.setScene(scene);
+                                stagePop.showAndWait();
+                            }
+                        } catch (IOException e) {
+                            System.err.println(String.format("Error: %s", e.getMessage()));
+                        }
+                    }
+                });
+            }
+        }).start();
     }
 
     @FXML
     void returnEquiBtnAction(ActionEvent event) {
-        try {
-            if (cargarDatos.isActivo()) {
-                FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("/GUI/views/returnEquipment.fxml"));
-                Scene scene = new Scene(fxmlLoader.load(), 390, 272);
-                Stage stagePop = new Stage();
-                stagePop.setTitle("Confirmar devolucion");
-                stagePop.getIcons().add(new Image(getClass().getResourceAsStream("/GUI/static/icons/herramienta.png")));
-                stagePop.setScene(scene);
-                stagePop.showAndWait();
-            } else {
-                FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("/GUI/views/noReturn.fxml"));
-                Scene scene = new Scene(fxmlLoader.load(), 390, 210);
-                Stage stagePop = new Stage();
-                stagePop.setTitle("Error");
-                stagePop.setScene(scene);
-                stagePop.getIcons().add(new Image(getClass().getResourceAsStream("/GUI/static/icons/error.png")));
-                stagePop.showAndWait();
-            }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            if (cargarDatos.isActivo()) {
+                                FXMLLoader fxmlLoader = new FXMLLoader();
+                                fxmlLoader.setLocation(getClass().getResource("/GUI/views/returnEquipment.fxml"));
+                                Scene scene = new Scene(fxmlLoader.load(), 390, 272);
+                                Stage stagePop = new Stage();
+                                stagePop.setTitle("Confirmar devolucion");
+                                stagePop.getIcons().add(new Image(getClass().getResourceAsStream("/GUI/static/icons/herramienta.png")));
+                                stagePop.setScene(scene);
+                                stagePop.showAndWait();
+                            } else {
+                                FXMLLoader fxmlLoader = new FXMLLoader();
+                                fxmlLoader.setLocation(getClass().getResource("/GUI/views/noReturn.fxml"));
+                                Scene scene = new Scene(fxmlLoader.load(), 390, 210);
+                                Stage stagePop = new Stage();
+                                stagePop.setTitle("Error");
+                                stagePop.setScene(scene);
+                                stagePop.getIcons().add(new Image(getClass().getResourceAsStream("/GUI/static/icons/error.png")));
+                                stagePop.showAndWait();
+                            }
 
-        } catch (IOException e) {
-            System.err.println(String.format("Error: %s", e.getMessage()));
-        }
+                        } catch (IOException e) {
+                            System.err.println(String.format("Error: %s", e.getMessage()));
+                        }
+                    }
+                });
+            }
+        }).start();
     }
 
     @FXML
