4f66c85b61d694323bb0a46148adfc1a77a4ee04
Julian Bustos <dbustos@unal.edu.co>
2020-12-06 21:40:22 -0500

    6/12/2020 9:40

--- src/GUI/controllers/ConfirmRequestController.java
+++ src/GUI/controllers/ConfirmRequestController.java
@@ -38,17 +38,15 @@ public class ConfirmRequestController implements Initializable {
 
     @FXML
     void loanBtnAction(ActionEvent event) throws IOException {
-        if (RP.computerIsFree(computador)) {
-            if (RP.makeBorrow(cargarDatosUsuario.getUser(), computador, SoftwareSelectedController.getSelectedProgramArr(), LocalDateTime.now(), LocalDateTime.now().plusHours(1))) {
-                System.out.println("SE hizo el prestamo!");
-                prestamo = true;
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        cargarDatosUsuario.cargar(cargarDatosUsuario.getUser());
-                    }
-                }).start();
-            }
+        if (RP.makeBorrow(cargarDatosUsuario.getUser(), computador, SoftwareSelectedController.getSelectedProgramArr(), LocalDateTime.now(), LocalDateTime.now().plusHours(1))) {
+            System.out.println("SE hizo el prestamo!");
+            prestamo = true;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    cargarDatosUsuario.cargar(cargarDatosUsuario.getUser());
+                }
+            }).start();
             Stage stage = (Stage) cancelRequestBtn.getScene().getWindow();
             stage.close();
         } else {
